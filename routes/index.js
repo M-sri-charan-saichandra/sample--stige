3ab8e730373268e1b9a62a353a5c95fcb8d4cc4c
M-sri-charan-saichandra <90337546+M-sri-charan-saichandra@users.noreply.github.com>
2021-11-05 21:27:28 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Class = require('../models/class');
 
-/* GET home page. */
+/* GET home page.  */
 router.get('/', function(req, res, next) {
   Class.getClasses(function(err, classes) {
     res.render('index', { title: 'Code Studio', classes: classes });
